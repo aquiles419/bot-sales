eb656a034635dd9b52ec866b10f58b020236ed87
Aquiles <wesley419@gmail.com>
2023-05-24 15:28:35 -0300

    feat: update env

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -4,5 +4,9 @@ declare namespace NodeJS {
     PORT: string;
     MONGO_URI: string;
     APP_SECRET: string;
+    AWS_ACCESS_KEY_ID: string;
+    AWS_SECRET_ACCESS_KEY: string;
+    AWS_REGION: string;
+    AWS_BUCKET_NAME: string;
   }
 }
