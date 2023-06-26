d5b1195f19151598dec483c016af1e27721b78e8
Aquiles <wesley419@gmail.com>
2023-06-26 16:10:23 -0300

    feat: add expense in model trip

--- src/modules/expenses/useCases/CreateExpenses/CreateExpensesUseCase.ts
+++ src/modules/expenses/useCases/CreateExpenses/CreateExpensesUseCase.ts
@@ -4,12 +4,15 @@ import { AppException } from "../../../../shared/exceptions/AppException";
 import { v4 as uuidV4 } from "uuid";
 import IExpensesDTO from "../../dtos/IExpensesDTO";
 import { IExpensesRepository } from "../../repositories/IExpensesRepository";
+import { ITripsRepository } from "../../../trips/repositories/ITripsRepository";
 
 @injectable()
 export class CreateExpensesUseCase {
   constructor(
     @inject("ExpensesRepository")
-    private expensesRepository: IExpensesRepository
+    private expensesRepository: IExpensesRepository,
+    @inject("TripsRepository")
+    private tripsRepository: ITripsRepository
   ) {}
 
   async execute(data: IExpensesDTO): Promise<Expense> {
@@ -25,13 +28,11 @@ export class CreateExpensesUseCase {
       );
     }
 
-    //add verification email
-
     const currentDate = new Date();
 
     const expenses: IExpensesDTO = {
       _id: data._id || uuidV4(),
-      trip_id: data._id,
+      trip_id: data.trip_id,
       description: data.description,
       value: data.value,
       payer: data.payer,
@@ -42,6 +43,12 @@ export class CreateExpensesUseCase {
     };
 
     const createdExpenses = await this.expensesRepository.create(expenses);
+
+    const trip: any = await this.tripsRepository.findById(data.trip_id);
+    if (trip) {
+      trip.expenses.push(expenses._id);
+      await this.tripsRepository.save(trip);
+    }
 
     return createdExpenses;
   }
